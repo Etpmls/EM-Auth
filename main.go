04603a91127187aa77115dc4419a1e4efbbdbfe9
Samsung T5 - Program <Samsung T5 - Program>
2020-12-01 15:30:47 +0800

    remove register.config

--- main.go
+++ main.go
@@ -4,13 +4,11 @@ import (
 	"github.com/Etpmls/EM-Auth/src/application/database"
 	"github.com/Etpmls/EM-Auth/src/register"
 	"github.com/Etpmls/Etpmls-Micro"
-	em_library "github.com/Etpmls/Etpmls-Micro/library"
 )
 
 
 func main() {
 	var reg = em.Register{
-		Config:             	em_library.Config,
 		GrpcServiceFunc:    	register.RegisterRpcService,
 		GrpcMiddleware:     	register.RegisterGrpcMiddleware,
 		HttpServiceFunc:    	register.RegisterHttpService,
