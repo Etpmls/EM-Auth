db9d0faf59ed587c327c373474dbb48b3896e304
Samsung T5 - Program <Samsung T5 - Program>
2020-12-21 16:24:38 +0800

    change route path

--- src/register/route.go
+++ src/register/route.go
@@ -12,5 +12,5 @@ func RegisterRoute(mux *runtime.ServeMux)  {
 	mux.HandlePath("GET", em_library.Config.ServiceDiscovery.Service.CheckUrl, func(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
 		w.Write([]byte("hello"))
 	})
-	mux.HandlePath("GET", "/api/test", service.ServiceAuth{}.Check)
+	mux.HandlePath("GET", "/api/checkAuth", service.ServiceAuth{}.Check)
 }
